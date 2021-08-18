d30822af5e5d794343b9838666ca627840fd0ab8
BlaChalk <black00731@gmail.com>
2021-08-18 10:02:50 +0800

    deploy

---
+++ precache-manifest.9aea3710409d49175a47b42399345b18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83270c49b3988573d7b0",
+    "url": "/mask-app/css/app.b417a13a.css"
+  },
+  {
+    "revision": "2c614f1a0c659df769811eef2fc68a3e",
+    "url": "/mask-app/index.html"
+  },
+  {
+    "revision": "83270c49b3988573d7b0",
+    "url": "/mask-app/js/app.f7e56c10.js"
+  },
+  {
+    "revision": "b8c7e53d9b3c8ce9e074",
+    "url": "/mask-app/js/chunk-vendors.cfa8cfda.js"
+  },
+  {
+    "revision": "67c977625e48dfdd74fb34095c1f4594",
+    "url": "/mask-app/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/mask-app/robots.txt"
+  }
+]);
